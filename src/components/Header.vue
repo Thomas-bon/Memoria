a60e15595d0f1c3edebc3c0a79e5162ab021f31c
SachaBouk <sachaboukouya@gmail.com>
2025-02-20 14:27:13 +0100

    modification of the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,33 +5,97 @@
   <header>
     <div class="BarHead">
       <div class="alignHeader">
-        <img src="./Icons/logo.png" alt="">
+        <div id="logo">
+          <a href="#"><img id="memoriaLogo" src="./Icons/logo.png" alt=""></a>
+        </div>
+        <div class="linksAlign">
+          <div id="boutique">
+            <a href="#">Boutique</a>
+          </div>
+          <div id="About">
+            <a href="#">About</a>
+          </div>
+          <div id="Contacts">
+            <a href="#">Contacts</a>
+          </div>
+        </div>
+        <div id="shopLogo">
+          <a href="#"><img id="shopBag" src="./Icons/shopLogo.svg" alt=""></a>
+        </div>
       </div>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
 
-header{
-  background-color: #f5f5f5;
+header {
+  background-color: green;
+  backdrop-filter: blur(2px);
+  -webkit-backdrop-filter: blur(2px);
   width: 100%;
   margin: 0;
+  position: fixed;
+  top: 0;
 }
 
-.BarHead{
+.BarHead {
   width: 100%;
   height: 10vh;
   display: flex;
+  flex-direction: row;
+  align-items: center;
   justify-content: center;
-  align-items:center ;
 }
 
-img{
+.alignHeader {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 380px;
+}
+
+
+
+.linksAlign {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 125px;
+
+}
+
+img {
   height: 75px;
   width: 75px;
+}
+
+#logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#shopBag {
+  height: 55px;
+  width: 55px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#memoriaLogo{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+  font-size: 2em;
 }
 
 </style>
