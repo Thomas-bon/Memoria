a96a7ece66b0e1cb4da9b2a182b4664e9113a460
SachaBouk <sachaboukouya@gmail.com>
2025-02-20 16:11:06 +0100

    create route for all pages

---
+++ src/views/ShopView.vue
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+
+<template>
+  <div class="Container">
+    <h1>sssgjyhkjkhjkhjhjhjhjjhjhss</h1>
+  </div>
+</template>
+
+<style scoped>
+h1{
+    color: red;
+}
+
+template{
+    height: 80vh;
+    width: 100%;
+}
+.Container{
+    top: 0;
+    margin: 0;
+}
+</style>
+
