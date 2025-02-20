fb3589c54dcc92bc7638de27e5d77ca475c506f3
Thomas-bon <pro.bonanseathomas@gmail.com>
2025-02-20 13:06:10 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script>
+</script>
+
+<template>
+  <header>
+    <div class="BarHead">
+      <div class="alignHeader">
+        <img src="./Icons/logo.png" alt="">
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+header{
+  background-color: #f5f5f5;
+  width: 100%;
+  margin: 0;
+}
+
+.BarHead{
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items:center ;
+}
+
+img{
+  height: 75px;
+  width: 75px;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,67 @@
+<script>
+
+</script>
+
+<template>
+
+  <div id="brand">
+
+    <h1 id="title">Memoria</H1>
+    <h2>“Gravez votre parcours, portez votre victoire
+      Mémoria, le t-shirt de votre marathon”</h2>
+    <button id="store">Voir boutique</button>
+
+  </div>
+
+  <div id="arrowSlide">
+    <div id="boxArrow">
+      <div id="arrow">
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  user-select: none;
+}
+
+template {
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+}
+
+#brand {
+  height: 100vh;
+  width: 100%;
+  /* background-color: red; */
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+#title {
+  font-size: 11em;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  color: rgb(0, 0, 0);
+  text-align: center;
+}
+
+#arrowSlide {
+  width: 100%;
+  height: 100px;
+  background-color: red;
+}
+</style>
